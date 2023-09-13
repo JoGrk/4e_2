c080d12f46ccb9a56c9ae2169c4497490aa83cae
Jola Gryko <jgryko@gmail.com>
2023-09-13 12:44:03 +0200

    js

---
+++ js/05_funkcje.js
@@ -0,0 +1,18 @@
+function regards(timeOfDay,name){
+    console.log(`good ${timeOfDay}, ${name}`)
+}
+
+regards('night','wojtas')
+
+
+function average(x,y){
+    return (x+y)/2
+
+}
+console.log(average(2,5));
+
+function triple(x){
+    return x+x+x;
+};
+
+console.log(triple('wojtas '))
