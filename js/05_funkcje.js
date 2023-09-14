ab7d6b4dc821d6e2e4dd94d655aed8cd28c931c1
Jola Gryko <jgryko@gmail.com>
2023-09-14 14:59:06 +0200

    js

--- js/05_funkcje.js
+++ js/05_funkcje.js
@@ -16,3 +16,34 @@ function triple(x){
 };
 
 console.log(triple('wojtas '))
+
+function isOdd(number){
+    return number%2!=0 ;
+    
+}
+console.log(isOdd(6));
+console.log(isOdd(5));
+
+function isDivided(a,b){
+    return a%b==0
+}
+
+console.log(isDivided(30,4))
+console.log(isDivided(30,5))
+
+function isLeapYear(year){
+    return isDivided(year,4) && !isDivided(year,100) || isDivided(year,400)
+}
+console.log(isLeapYear(2000))
+console.log(isLeapYear(2023))
+
+function halfOf(number){
+    return number/2;
+}
+console.log(halfOf(2))
+console.log(halfOf(2137))
+
+function IsLongerThan(text,number){
+    return text.length>number;
+}
+console.log(IsLongerThan(`sos`,2))
