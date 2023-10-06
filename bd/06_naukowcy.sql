1a61cfafbaa1df2b2d7db61c0b17c65917243773
Jola Gryko <jgryko@gmail.com>
2023-10-06 13:42:31 +0200

    sql

--- bd/06_naukowcy.sql
+++ bd/06_naukowcy.sql
@@ -59,56 +59,64 @@ DROP Miejsce;
 
 -- 9. W tabeli Pracownicy zmień pole Nazwisko - powinno być tekstem o zmiennej długości do 50 znaków
 
+ALTER TABLE Pracownicy
+MODIFY Nazwisko varchar(50);
 -- 10. W tabeli Projekty ustaw wartość domyślną na polu Czas na 100.
-
+ALTER TABLE Projekty
+ALTER Czas SET DEFAULT(100); 
 -- 11. Zmień nazwę tabeli Pracownicy na Naukowcy.
-
+ALTER TABLE Pracownicy
+RENAME TO Naukowcy;
 -- 12. W tabeli Przypisanie przesuń pole Projekt za pole SSN.
-
+ALTER TABLE Przypisanie
+MODIFY Projekt char(4) after SSN;
 -- 14. W tabeli Przypisanie zmień nazwę pola SSN na Naukowiec
-
+ALTER TABLE Przypisanie
+ CHANGE SSN Naukowiec varchar(255);
 -- 15. Z tabeli Naukowcy usuń pole Data_urodzenia
+ALTER TABLE Naukowcy
+DROP Data_urodzenia;
 
 -- 16. Zaimportuj dane
 
--- INSERT INTO Naukowcy(Kod,Nazwisko) 
---   VALUES(123234877,'Michael Rogers'),
---     (152934485,'Anand Manikutty'),
---     (222364883, 'Carol Smith'),
---     (326587417,'Joe Stevens'),
---     (332154719,'Mary-Anne Foster'),	
---     (332569843,'George ODonnell'),
---     (546523478,'John Doe'),
---     (631231482,'David Smith'),
---     (654873219,'Zacary Efron'),
---     (745685214,'Eric Goldsmith'),
---     (845657245,'Elizabeth Doe'),
---     (845657246,'Kumar Swamy');
---  INSERT INTO Projekty( Kod,Nazwa,Czas)
---  VALUES ('AeH1','Winds: Studying Bernoullis Principle', 156),
---        ('AeH2','Aerodynamics and Bridge Design',189),
---        ('AeH3','Aerodynamics and Gas Mileage', 256),
---        ('AeH4','Aerodynamics and Ice Hockey', 789),
---        ('AeH5','Aerodynamics of a Football', 98),
---        ('AeH6','Aerodynamics of Air Hockey',89),
---        ('Ast1','A Matter of Time',112),
---        ('Ast2','A Puzzling Parallax', 299),
---        ('Ast3','Build Your Own Telescope', 6546),
---        ('Bte1','Juicy: Extracting Apple Juice with Pectinase', 321),
---        ('Bte2','A Magnetic Primer Designer', 9684),
---        ('Bte3','Bacterial Transformation Efficiency', 321),
---        ('Che1','A Silver-Cleaning Battery', 545),
---        ('Che2','A Soluble Separation Solution', 778);
---  INSERT INTO Przypisanie( Naukowiec, Projekt)
---    VALUES (123234877,'AeH1'),
---     (152934485,'AeH3'),
---     (222364883,'Ast3'),	   
---     (326587417,'Ast3'),
---     (332154719,'Bte1'),
---     (546523478,'Che1'),
---     (631231482,'Ast3'),
---     (654873219,'Che1'),
---     (745685214,'AeH3'),
---     (845657245,'Ast1'),
---     (845657246,'Ast2'),
---     (332569843,'AeH4');
+INSERT INTO Naukowcy(Kod,Nazwisko) 
+  VALUES(123234877,'Michael Rogers'),
+    (152934485,'Anand Manikutty'),
+    (222364883, 'Carol Smith'),
+    (326587417,'Joe Stevens'),
+    (332154719,'Mary-Anne Foster'),	
+    (332569843,'George ODonnell'),
+    (546523478,'John Doe'),
+    (631231482,'David Smith'),
+    (654873219,'Zacary Efron'),
+    (745685214,'Eric Goldsmith'),
+    (845657245,'Elizabeth Doe'),
+    (845657246,'Kumar Swamy');
+ INSERT INTO Projekty( Kod,Nazwa,Czas)
+ VALUES ('AeH1','Winds: Studying Bernoullis Principle', 156),
+       ('AeH2','Aerodynamics and Bridge Design',189),
+       ('AeH3','Aerodynamics and Gas Mileage', 256),
+       ('AeH4','Aerodynamics and Ice Hockey', 789),
+       ('AeH5','Aerodynamics of a Football', 98),
+       ('AeH6','Aerodynamics of Air Hockey',89),
+       ('Ast1','A Matter of Time',112),
+       ('Ast2','A Puzzling Parallax', 299),
+       ('Ast3','Build Your Own Telescope', 6546),
+       ('Bte1','Juicy: Extracting Apple Juice with Pectinase', 321),
+       ('Bte2','A Magnetic Primer Designer', 9684),
+       ('Bte3','Bacterial Transformation Efficiency', 321),
+       ('Che1','A Silver-Cleaning Battery', 545),
+       ('Che2','A Soluble Separation Solution', 778);
+ INSERT INTO Przypisanie( Naukowiec, Projekt)
+   VALUES (123234877,'AeH1'),
+    (152934485,'AeH3'),
+    (222364883,'Ast3'),	   
+    (326587417,'Ast3'),
+    (332154719,'Bte1'),
+    (546523478,'Che1'),
+    (631231482,'Ast3'),
+    (654873219,'Che1'),
+    (745685214,'AeH3'),
+    (845657245,'Ast1'),
+    (845657246,'Ast2'),
+    (332569843,'AeH4');
