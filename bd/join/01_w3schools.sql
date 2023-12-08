e9c5fc12e1f47f89b5463636c7bcea7fc2931cf4
Jola Gryko <jgryko@gmail.com>
2023-12-08 11:51:00 +0100

    php

--- bd/join/01_w3schools.sql
+++ bd/join/01_w3schools.sql
@@ -11,13 +11,30 @@ FROM Shippers
 FROM Employees
 	INNER JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID;
 -- 4. Wyświetl nazwy towarów, nazwy kategorii
+SELECT ProductName,CategoryName
+FROM Products
+	INNER JOIN Categories ON Products.CategoryID=Categories.CategoryID
  
 -- 5. Wyświetl nazwy towarów, nazwy kategorii i idzamówień
- 
+ SELECT ProductName,CategoryName,OrderDetails.OrderID OrderDate, CustomerID
+FROM Products
+	INNER JOIN Categories ON Products.CategoryID=Categories.CategoryID
+    INNER JOIN OrderDetails ON OrderDetails.ProductID=Products.ProductID
+    INNER JOIN Orders ON OrderDetails.OrderID=Orders.OrderID;
 -- 6. Wyświetl nazwy towarów, nazwy kategorii, daty zamówień i idklienta
  
 -- 7. Wyświetl nazwy towarów, nazwy kategorii, daty zamówień i nazwę firmy klienta
+SELECT ProductName,CategoryName,OrderDetails.OrderID OrderDate, Customers.CustomerID, CustomerName
+FROM Products
+	INNER JOIN Categories ON Products.CategoryID=Categories.CategoryID
+    INNER JOIN OrderDetails ON OrderDetails.ProductID=Products.ProductID
+    INNER JOIN Orders ON OrderDetails.OrderID=Orders.OrderID
+    INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
  
 -- 8. Ile razy był zamawiany dany produkt? (podaj nazwy produktów)
  
+ SELECT ProductName, COUNT(*)
+FROM OrderDetails
+INNER JOIN Products ON OrderDetails.ProductID = Products.ProductID
+GROUP BY ProductName
 -- 9. Wyświetl nazwy firm klientów, daty zamówień i nazwy towarów
