0ee6e6494835c952fe828506bc74f89a93049f76
Jola Gryko <jgryko@gmail.com>
2024-01-04 11:47:03 +0100

    exam_js

--- exam/js/galeria miejsc - EE.09-07-22.01-SG/skrypt.js
+++ exam/js/galeria miejsc - EE.09-07-22.01-SG/skrypt.js
@@ -1,6 +1,7 @@
 const mainImgE = document.querySelector('main img')
 const leftImgTable = document.querySelectorAll('#left img')
 const serduszkoE = document.querySelector('#right img')
+let serduszkoOn = false 
 for(let leftImgE of leftImgTable){
     leftImgE.addEventListener('click',(event)=>{
         mainImgE.src=leftImgE.src
@@ -8,5 +9,11 @@ for(let leftImgE of leftImgTable){
 }
 
     serduszkoE.addEventListener('click',(event)=>{
-        
+        serduszkoOn = !serduszkoOn
+        if(serduszkoOn){ 
+            serduszkoE.src= 'icon-on.png'
+        }
+        else{
+            serduszkoE.src = 'icon-off.png'
+        }
     })
