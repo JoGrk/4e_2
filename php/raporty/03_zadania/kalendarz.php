66d41b0ee09a6f5b0aaf724fbb09f60d32e20ad1
Jola Gryko <jgryko@gmail.com>
2024-01-05 10:57:54 +0100

    php

--- php/raporty/03_zadania/kalendarz.php
+++ php/raporty/03_zadania/kalendarz.php
@@ -5,16 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mój kalendarz</title>
-    <link rel="stylesheet" href="style5.css">
+    <link rel="stylesheet" href="styl5.css">
 </head>
 <body>
     <header>
-        <section id='left'></section>
-        <section id="right"></section>
+        <section id='left'>
+            <img src="logo1.png" alt="Mój kalendarz">
+
+        </section>
+        <section id="right">
+            <h1>KALENDARZ</h1>
+            <!-- skrpyt1 -->
+            <?php
+                $conn = new mysqli ('localhost', 'root', '', '4e_2_egzamin5');
+
+                $sql = "SELECT miesiac,rok
+                FROM zadania
+                WHERE dataZadania='2020-07-01';";
+
+                $result = $conn -> query($sql);
+
+                while($row=$result -> fetch_assoc(())){
+
+                }
+
+                $conn -> close();
+            ?>
+        </section>
     </header>
 
-    <main></main>
+    <main>
+        <!-- skrpyt2 -->  
+    </main>
     
-    <footer></footer>
+    <footer>
+        <form action="Kalendarz.php" method="POST">
+            <label for="">dodaj wpis</label>
+            <input type="text">
+            <button>DODAJ</button>
+        </form>
+
+        <p>Stronę wykonał: 00000000000</p>
+    </footer>
 </body>
 </html>
