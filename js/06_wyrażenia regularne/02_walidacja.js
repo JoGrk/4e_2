0ddf00f3e4905cb0e26f527baf6137b437e2bbba
Jolanta Gryko <jgryko@gmail.com>
2024-01-18 11:56:36 +0100

    js_reg

--- js/06_wyrażenia regularne/02_walidacja.js
+++ js/06_wyrażenia regularne/02_walidacja.js
@@ -7,5 +7,27 @@ const zipcodeE = document.getElementById(`zipcode`)
 const zipcodeErrorE = document.getElementById(`zipcode-error`)
 const addresE = document.getElementById(`addres`)
 const addresErrorE = document.getElementById(`addres-error`)
-const resE = getElementById(`res`)
+const resE = document.getElementById(`res`)
 
+
+butE.addEventListener('click',(e)=>{
+    e.preventDefault()
+
+    const emailReg = /^\w{4,}@staszic.eu$/i
+
+    
+    if(surnameE.value.length < 3){
+        surnameErrorE.innerHTML="zbyt krótkie"
+        surnameE.style.backgroundColor="pink"
+    }
+    else(
+        resE.innerHTML=surnameE.value +"<br>"
+        
+    )
+
+    if(!emailReg.test(emailE.value)){}
+     
+
+
+
+})
