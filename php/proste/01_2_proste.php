e82367acabf5ee6813e77a07fa4272b80aedce60
Jolanta Gryko <jgryko@gmail.com>
2024-04-08 12:43:46 +0200

    php

--- php/proste/01_2_proste.php
+++ php/proste/01_2_proste.php
@@ -13,7 +13,35 @@
     echo "<p class='big'> $twinkle, $twinkle little $star </p>";
     echo "<p class='bigger'> $twinkle, $twinkle little $star </p>";
     echo "<p class='thebigest'> $twinkle, $twinkle little $star </p>";
+    $x=10;
+    $y=-7;
+    $z=$x-$y;
+    echo "<p> $x+$y=".$x+$y." </p>";
+    echo "<p>$x-$y=$z </p>";
     
+    $liczba = 8;
+    echo "<p> liczba wynosi $liczba </p>";
+    $liczba+=2;
+    echo "<p> liczba wynosi $liczba </p>";
+    $liczba-=4;
+    echo "<p> liczba po zmniejszeniu o 4 wynosi teraz $liczba";
+    $liczba*=5;
+    echo "<p> liczba po pomnożeniu przez 5 wynosi $liczba";
+
+    $liczba /= 3;
+    echo "<p> liczba wynosi $liczba </p>";
+
+    $liczba ++;
+    echo "<p> liczba po inkrementacji jest rowna $liczba </p>";
+    $liczba --;
+    echo "<p>zmniejsz o jeden liczba po dekrementacji jest równa 10</p>";
+
+    $around = "around";
+    echo 'What goes '.$around.' comes '.$around."<br>";
+    
+
+    echo $around." jest typu ".gettype($around)."<br>";
+
     ?>
 </body>
 </html>
